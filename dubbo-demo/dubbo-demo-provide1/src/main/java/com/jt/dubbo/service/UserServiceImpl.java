551f0e151b8b8b838f33e5f1b03c8d99c73533a7
000 <000@eureka1>
2019-05-07 15:01:28 +0800

    1

---
+++ dubbo-demo/dubbo-demo-provide1/src/main/java/com/jt/dubbo/service/UserServiceImpl.java
@@ -0,0 +1,27 @@
+package com.jt.dubbo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.jt.dubbo.mapper.UserMapper;
+import com.jt.dubbo.pojo.User;
+@Service //必须使用dubbo注解	
+public class UserServiceImpl implements UserService {
+	
+	@Autowired
+	private UserMapper userMapper;
+
+	@Override
+	public List<User> findAll() {
+		
+		return userMapper.selectList(null);
+	}
+
+	@Override
+	public String getMsg() {
+		
+		return "我是第一台9000提供者";
+	}
+}
